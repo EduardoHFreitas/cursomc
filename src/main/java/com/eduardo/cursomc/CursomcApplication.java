48daf81c1a31ebb7c774b68f0a43cbb284017bbc
Eduardo <duh.henrike@gmail.com>
2021-02-16 16:19:52 -0300

    Criacao da entidade Produto e associacao Muitos pra Muitos com a Categoria

--- src/main/java/com/eduardo/cursomc/CursomcApplication.java
+++ src/main/java/com/eduardo/cursomc/CursomcApplication.java
@@ -1,8 +1,6 @@
 package com.eduardo.cursomc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,15 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.eduardo.cursomc.domain.Categoria;
+import com.eduardo.cursomc.domain.Produto;
 import com.eduardo.cursomc.repositories.CategoriaRepository;
-
-import ch.qos.logback.core.joran.spi.NoAutoStart;
+import com.eduardo.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
-	private CategoriaRepository repo;
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -29,7 +30,19 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 		
-		repo.saveAll(Arrays.asList(cat1, cat2));
+		Produto Prod1 = new Produto(null, "Computador", 2000.00);
+		Produto Prod2 = new Produto(null, "Impressora", 800.00);
+		Produto Prod3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(Prod1, Prod2, Prod3));
+		cat2.getProdutos().add(Prod2);
+		
+		Prod1.getCategorias().add(cat1);
+		Prod2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		Prod3.getCategorias().add(cat1);
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(Prod1, Prod2, Prod3));
 	}
 
 }
