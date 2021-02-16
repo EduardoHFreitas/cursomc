fa6827d83b6d439d0c9b48a622e0208ab9b5a428
Eduardo <duh.henrike@gmail.com>
2021-02-16 15:41:18 -0300

    Criando operacao de Instanciacao

--- src/main/java/com/eduardo/cursomc/CursomcApplication.java
+++ src/main/java/com/eduardo/cursomc/CursomcApplication.java
@@ -1,13 +1,35 @@
 package com.eduardo.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.eduardo.cursomc.domain.Categoria;
+import com.eduardo.cursomc.repositories.CategoriaRepository;
 
+import ch.qos.logback.core.joran.spi.NoAutoStart;
+
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		repo.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
