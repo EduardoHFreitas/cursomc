24bae288b38e81fd4664511f103432899748202b
Eduardo <duh.henrike@gmail.com>
2021-02-16 14:36:55 -0300

    Criando entidade Categoria e alterando Resource da mesma

--- src/main/java/com/eduardo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/eduardo/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.eduardo.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.eduardo.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Ok";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.addAll(Arrays.asList(cat1, cat2));
+		
+		return lista;
 	}
 }
