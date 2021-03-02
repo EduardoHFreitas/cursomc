74344ba009b00b7c1fb370831203b9a18e67ff22
Eduardo <duh.henrike@gmail.com>
2021-03-02 17:29:54 -0300

    Validacao de email ja cadastrado na insercao de cliente

--- src/main/java/com/eduardo/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/eduardo/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.eduardo.cursomc.domain.Cliente;
 import com.eduardo.cursomc.domain.enums.TipoCliente;
 import com.eduardo.cursomc.dto.ClienteDTO;
+import com.eduardo.cursomc.repositories.ClienteRepository;
 import com.eduardo.cursomc.resources.exceptions.FieldMessage;
 import com.eduardo.cursomc.services.validation.utils.DocumentUtil;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsertAnnotation, ClienteDTO> {
 
+	@Autowired
+	private ClienteRepository clienteRepository; 
+	
 	@Override
 	public void initialize(ClienteInsertAnnotation ann) {	
 	}
@@ -30,6 +37,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			erros.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 
+		Cliente cliente = clienteRepository.findByEmail(clienteDTO.getEmail());
+		
+		if (cliente != null) {
+			erros.add(new FieldMessage("email", "Email já cadastrado"));
+		}
+		
 		for (FieldMessage e : erros) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getErro()).addConstraintViolation();
