7f013858f14ff1a971e95e0ed216eb075454225c
Eduardo <duh.henrike@gmail.com>
2021-04-05 17:00:31 -0300

    implementando autorizacao

--- src/main/java/com/eduardo/cursomc/security/JWTUtil.java
+++ src/main/java/com/eduardo/cursomc/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -23,5 +24,39 @@ public class JWTUtil {
 				.setExpiration(new Date(System.currentTimeMillis() + expiration))
 				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
 				.compact();
+	}
+
+	public boolean isTokenValido(String token) {
+		Claims claims = getClaims(token);
+		
+		if (claims != null) {
+			String email = claims.getSubject();
+			Date expiration = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+			
+			if (email != null && expiration != null && now.before(expiration)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+	public String getEmail(String token) {
+		Claims claims = getClaims(token);
+		
+		if (claims != null && claims.getSubject() != null) {
+			return claims.getSubject();
+		}
+		
+		return null;
+	}
+	
+	private Claims getClaims(String token) {
+		try {
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+		} catch (Exception e) {
+			return null;
+		}
 	}
 }
