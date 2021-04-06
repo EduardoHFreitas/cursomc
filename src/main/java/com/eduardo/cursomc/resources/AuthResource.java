f23c126d06f852b042c674b01ae8b8f98109aa0a
Eduardo <duh.henrike@gmail.com>
2021-04-06 14:46:08 -0300

    Disponibilizando endpoint para refresh token

---
+++ src/main/java/com/eduardo/cursomc/resources/AuthResource.java
@@ -0,0 +1,31 @@
+package com.eduardo.cursomc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eduardo.cursomc.security.JWTUtil;
+import com.eduardo.cursomc.security.User;
+import com.eduardo.cursomc.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		User user = UserService.getAuthenticatedUser();
+		String token = jwtUtil.generateToken(user.getUsername());
+		
+		response.addHeader("Authorization", "Bearer " + token);
+		
+		return ResponseEntity.noContent().build();
+	}
+}
